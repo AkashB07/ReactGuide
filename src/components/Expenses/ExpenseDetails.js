96755f5f5fd43204f9de8469d6c5d5dda4d5dd4c
AkashB07 <akashbadhi7@gmail.com>
2023-01-17 13:13:07 +0530

    Adding Event Listeners made easy

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./ExpenseDetails.css";
 
-const ExpenseDetails = (props) => {
+function ExpenseDetails(props) {
+  const clickHandler = () => {
+    console.log('Clicked!!')
+  }
   return (
     <div className="expense-item__description">
       <h2>{props.title}</h2>
       <h2>{props.location}</h2>
       <div className="expense-item__price">Rs {props.amount}</div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button>Delete Expense</button>
     </div>
+   
   );
 }
 
