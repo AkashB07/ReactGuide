b28facbedf458e18705f72b2130d36ae656f4223
AkashB07 <akashbadhi7@gmail.com>
2023-01-17 18:31:30 +0530

    PlaceHolder

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,25 +1,11 @@
-import React, {useState} from "react";
+import React from "react";
 import "./ExpenseDetails.css";
 
 function ExpenseDetails(props) {
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle('Updated!');
-    console.log(title);
-  };
-
-  const [amount, setAmount] = useState(props.amount);
-  const editAmount = () => {
-    setAmount(100);
-    console.log(amount);
-  };
-
   return (
     <div className="expense-item__description">
-      <h2>{title}</h2>
-      <div className="expense-item__price">Rs {amount}</div>
-      <button onClick={clickHandler}>Change Title</button>
-      <button onClick={editAmount}>Change Amount</button>
+      <h2>{props.title}</h2>
+      <div className="expense-item__price">Rs {props.amount}</div>
     </div>
    
   );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,20 @@
+import React, { useState } from 'react';
+
 import ExpenseItem from "./ExpenseItem";
-import React from "react";
 import Card from "../UI/Card";
-import "./Expenses.css";
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       {props.items.map((expense) => {
         return (
           <ExpenseItem
